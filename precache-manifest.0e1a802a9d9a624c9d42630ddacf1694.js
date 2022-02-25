cfbfce12a91ac859faa2d6c42fca916a148050a6
SeddikBellamine <bellamine.seddik@gmail.com>
2022-02-25 14:19:09 +0100

    Updates

---
+++ precache-manifest.0e1a802a9d9a624c9d42630ddacf1694.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17574eb54bbdc6c8a8f4af4707e439fa",
+    "url": "/confidential-nft/index.html"
+  },
+  {
+    "revision": "cbc9025c9e6b3330718e",
+    "url": "/confidential-nft/static/css/2.25dbda51.chunk.css"
+  },
+  {
+    "revision": "94ced65d3dda25d6a3fa",
+    "url": "/confidential-nft/static/css/main.cf42dbf6.chunk.css"
+  },
+  {
+    "revision": "cbc9025c9e6b3330718e",
+    "url": "/confidential-nft/static/js/2.c78773ac.chunk.js"
+  },
+  {
+    "revision": "9f8682dc8cf2ee3f84ec031134d8f325",
+    "url": "/confidential-nft/static/js/2.c78773ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ced65d3dda25d6a3fa",
+    "url": "/confidential-nft/static/js/main.4c747ae7.chunk.js"
+  },
+  {
+    "revision": "565f50113f350bc42c23",
+    "url": "/confidential-nft/static/js/runtime-main.b7f9df07.js"
+  },
+  {
+    "revision": "6c5b9c38aeeb3cb156882edcc1c9a2b9",
+    "url": "/confidential-nft/static/media/space-mono-all-700-normal.6c5b9c38.woff"
+  },
+  {
+    "revision": "cb5b1fadc6b2892596bf89816d2eb8fd",
+    "url": "/confidential-nft/static/media/space-mono-latin-700-normal.cb5b1fad.woff2"
+  },
+  {
+    "revision": "a942d504a244e1172c28406d6f192df4",
+    "url": "/confidential-nft/static/media/space-mono-latin-ext-700-normal.a942d504.woff2"
+  },
+  {
+    "revision": "6accba19faa55ba9909d8d5f94b8e570",
+    "url": "/confidential-nft/static/media/space-mono-vietnamese-700-normal.6accba19.woff2"
+  }
+]);
